51615a4fcb8a1b5f36d94bb1744c0f396240a8b1
R1das67 <heinrichleon09@gmail.com>
2025-07-16 00:09:42 +0200

    keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "✅ Bot ist online!"
+    return "✅ Bot läuft!"
 
 def run():
-    port = int(os.environ.get("PORT", 8080))  # Render-kompatibel!
+    port = int(os.environ.get("PORT", 8080))  # Render holt hier den richtigen Port
     app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
